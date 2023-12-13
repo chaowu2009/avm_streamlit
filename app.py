e7994e99eb2ca3b0fc3a65fcfdb89ef391efd837
Chao Wu <chaowu2009@gmail.com>
2023-12-12 20:44:27 -0500

    updating dl_model.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pylab as plt
 import seaborn as sns
-
+from dl_model import train_dl_model
 
 st.title("AVM example with streamlit")
 
@@ -113,23 +113,23 @@ st.pyplot(fig)
 
 st.title("Try different machine learing algorithms")
 
-slider_values = st.slider('Select a range of values (in millions)', 0, 10, (1, 6))
+slider_values = st.slider('Select a range of values (in millions)', 0, 100, (1, 20))
 lower_threshold= slider_values[0]
 upper_threshold = slider_values[1]
 
-lower_threshold = float(lower_threshold)*1e5
-upper_threshold = float(upper_threshold)*1e6
+lower_threshold = float(lower_threshold)*1e4  #using different scale here, 1e4 = 0.01 million 
+upper_threshold = float(upper_threshold)*1e5  #20 equals to 20e5 = 2 million
 
 st.write(f"selected threshold (million) = {lower_threshold/1e6}, {upper_threshold/1e6}")
 
-
 f1 = df['price'] < upper_threshold
 f2 = df['price'] > lower_threshold
+
+st.write(f"before trimming: length(df)= {len(df)}")
+
 df = df[f1 & f2]
 
-st.write(f"length(df)= {len(df)}")
-
-X = df.drop(['price'],axis =1).values
+st.write(f"after trimming: length(df)= {len(df)}")
 
 # convert price to log10 scale
 import streamlit_toggle as toggle 
@@ -141,23 +141,46 @@ log_scale = toggle.st_toggle_switch(label="log scale",
                     active_color="#11567f", 
                     track_color="#29B5E8"
                     )
+
+log_scale = False
 print(f"log_scale = {log_scale}")
+df_original = df.copy()
 if log_scale:
-    df['price'] = np.log10(df['price'])
+    df['price'] = np.log10(df_original['price'])
+else:
+    df['price'] = df_original['price']
 
-
+X = df.drop(['price'],axis =1).values
 y = df['price'].values
 
-#splitting Train and Test 
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=888)
+def prepare_training_data(df):
+    X = df.drop(['price'],axis =1).values
+    y = df['price'].values
 
-# normalize
-from sklearn.preprocessing import StandardScaler
-s_scaler = StandardScaler()
-X_train = s_scaler.fit_transform(X_train.astype(float))
-X_test = s_scaler.transform(X_test.astype(float))
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=888)
 
+    # normalize
+    from sklearn.preprocessing import StandardScaler
+    s_scaler = StandardScaler()
+    X_train = s_scaler.fit_transform(X_train.astype(float))
+    X_test = s_scaler.transform(X_test.astype(float))
+
+    return X_train, X_test, y_train, y_test
+
+def convert_df_to_np_data(df):
+    X = df.drop(['price'],axis =1).values
+    y = df['price'].values
+
+    # normalize
+    from sklearn.preprocessing import StandardScaler
+    s_scaler = StandardScaler()
+    X = s_scaler.fit_transform(X.astype(float))
+    
+    return X, y
+
+
+X_train, X_test, y_train, y_test = prepare_training_data(df)
 
 st.subheader("Linear Regression")
 #Liner Regression
@@ -166,26 +189,35 @@ lr_model = LinearRegression()
 lr_model.fit(X_train, y_train)
 
 # compute PPE 10
-def compute_ppe10(df):
-    df['predicted'] = np.power(10, df['predicted'])
-    df['soldprice'] = np.power(10, df['soldprice'])
 
-    df['error'] = (df['predicted']-df['soldprice'])/df['soldprice']
+def compute_ppe10(df_input):
+    if log_scale:
+        df_input['predicted'] = np.power(10, df_input['predicted'])
+        df_input['soldprice'] = np.power(10, df_input['soldprice'])
+
+    df_input['error'] = (df_input['predicted']-df_input['soldprice'])/df_input['soldprice']
+
+    f1 = np.abs(df_input['error'])<=0.1
     
-    ppe10 = round(len(df[np.abs(df['error'])<=0.1])/len(df)*100,2)
+    ppe10 = round(len(df_input[f1])/len(df_input)*100,2)
 
     return ppe10
 
-def compute_stat(model):
-    y_pred = model.predict(X_train)
+def compute_stat(model, X_train, X_test, y_train, y_test, dl_model_flag=0):
+    if dl_model_flag==1:
+        y_pred_1 = model.predict(X_train)[:,0]
+        y_pred_2 = model.predict(X_test)[:,0]
+    else:
+        y_pred_1 = model.predict(X_train)
+        y_pred_2 = model.predict(X_test)
     #st.write(y_pred)
     df_train= pd.DataFrame({'soldprice': y_train})
-    df_train['predicted'] =  y_pred
+    df_train['predicted'] =  y_pred_1
 
     #evaluate the model (intercept and slope)
-    y_pred = model.predict(X_test)
+    
     df_test = pd.DataFrame({'soldprice': y_test})
-    df_test['predicted'] = y_pred
+    df_test['predicted'] = y_pred_2
 
     train_ppe10 = compute_ppe10(df_train)
     test_ppe10 = compute_ppe10(df_test)
@@ -198,7 +230,7 @@ def compute_stat(model):
     
     st.write(f"Train: PPE10 = {train_ppe10}, Test: PPE10 = {test_ppe10}")
 
-compute_stat(model = lr_model)
+compute_stat(lr_model,  X_train, X_test, y_train, y_test)
 
 st.subheader("Random Forest model")
 from sklearn.ensemble import RandomForestRegressor
@@ -206,19 +238,19 @@ from sklearn.ensemble import RandomForestRegressor
 rf_model = RandomForestRegressor()  
 rf_model.fit(X_train, y_train)
 
-compute_stat(model = rf_model)
+compute_stat(rf_model,  X_train, X_test, y_train, y_test)
 
-st.subheader("Linear GAM model")
+st.subheader("GAM model")
 from pygam import LinearGAM
 #gam = LinearGAM().fit(X, y)
 #gam = LinearGAM().gridsearch(X_train, y_train)
 gam_model = LinearGAM(n_splines=10).gridsearch(X_train, y_train)
 
-compute_stat(model = gam_model)
+compute_stat(gam_model,  X_train, X_test, y_train, y_test)
 
 
 st.subheader("xgboost model")
-def xgboost_model():
+def train_xgboost_model( X_train, X_test, y_train, y_test):
     import xgboost as xgb
     dtrain_reg = xgb.DMatrix(X_train, y_train, enable_categorical=True)
     dtest_reg = xgb.DMatrix(X_test, y_test, enable_categorical=True)
@@ -253,58 +285,60 @@ def xgboost_model():
 
     st.write(f"Train: PPE10 = {train_ppe10}, Test: PPE10 = {test_ppe10}")
 
-xgboost_model()
+train_xgboost_model( X_train, X_test, y_train, y_test)
 
 
 st.subheader("Catboost model")
 
 from catboost import CatBoostRegressor
-catboost_model = CatBoostRegressor(iterations=2,
-                          learning_rate=1,
-                          depth=2)
+catboost_model = CatBoostRegressor(iterations=20,
+                          learning_rate=0.01)
 # Fit model
 catboost_model.fit(X_train, y_train)
-# Get predictions
+# Get prediction errors
 
-compute_stat(model=catboost_model)
+compute_stat(catboost_model,  X_train, X_test, y_train, y_test)
 
 st.subheader("Deep Learing Model")
-# Creating a Neural Network Model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
-from tensorflow.keras.optimizers import Adam
 
-import os
-import tensorflow as tf
-#os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" #If the line below doesn't work, uncomment this line (make sure to comment the line below); it should help.
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+foundation_dl_model = train_dl_model(X_train, X_test, y_train, y_test)
 
-dl_model = Sequential()
+compute_stat(foundation_dl_model,  X_train, X_test, y_train, y_test, 1)
 
-input_dimesion = 18
+st.subheader("using the pretrained foundation model")
+st.markdown("saving the model to files")
+model_json = foundation_dl_model.to_json()
+with open("foundation_model.json","w") as json_file:
+    json_file.write(model_json)
 
-dl_model.add(Dense(input_dimesion, activation='relu'))
-dl_model.add(Dense(input_dimesion, activation='relu'))
-dl_model.add(Dense(input_dimesion, activation='relu'))
-dl_model.add(Dense(input_dimesion, activation='relu'))
-dl_model.add(Dense(1))
+foundation_dl_model.save_weights("foundation_model.h5")
 
-dl_model.compile(optimizer='adam',loss='mse')
+st.markdown("loading model from files")
+from tensorflow.keras.models import model_from_json
+with open("foundation_model.json","r") as fp:
+    loaded_model_json = fp.read()
 
-#st.write(X_train.shape)
+loaded_model =model_from_json(loaded_model_json)
+loaded_model.load_weights("foundation_model.h5")
+loaded_model.compile(optimizer='adam', loss='mse')
 
-dl_model.fit(x=X_train,y=y_train,
-          validation_data=(X_test,y_test),
-          batch_size=128,epochs=20)
+st.markdown("# train on a specific zipcode")
+
+df_2 = df[df['zipcode']==98103]
+
+X_train_2, y_train_2 = convert_df_to_np_data(df_2)
+
+loaded_model.fit(X_train_2, y_train_2, epochs=200, batch_size=64)
+
+#compute_stat(loaded_model, X_train_2, y_train_2, X_test, y_test, 1)
+y_pred_1 = loaded_model.predict(X_train_2)[:,0]
+
+df_train= pd.DataFrame({'soldprice': y_train_2})
+df_train['predicted'] =  y_pred_1
+
+train_ppe10 = compute_ppe10(df_train)
+st.write(f"new train PPE10 = {train_ppe10}")
 
 
-loss_df = pd.DataFrame(dl_model.history.history)
-fig = plt.figure()
-plt.plot(loss_df)
-plt.grid(True)
-plt.title('loss function')
-st.pyplot(fig)
-
-compute_stat(model=dl_model)
 
 
---
+++ dl_model.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pylab as plt
+
+
+def train_dl_model(X_train, X_test, y_train, y_test):
+
+    # Creating a Neural Network Model
+    from keras.models import Sequential
+    from keras.layers import Dense, Activation, Dropout
+
+
+    import os
+    import tensorflow as tf
+    #os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID" #If the line below doesn't work, uncomment this line (make sure to comment the line below); it should help.
+    os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+    dl_model = Sequential()
+
+    input_dimesion = 20
+
+    dl_model.add(Dense(input_dimesion, activation='relu'))
+    dl_model.add(Dropout(0.2))
+                 
+    dl_model.add(Dense(input_dimesion, activation='relu'))
+    dl_model.add(Dropout(0.2))
+                 
+    dl_model.add(Dense(input_dimesion, activation='relu'))
+    dl_model.add(Dropout(0.2))
+                 
+    dl_model.add(Dense(input_dimesion, activation='relu'))
+    dl_model.add(Dense(1))
+
+    dl_model.compile(optimizer='adam',loss='mse')
+
+    #st.write(X_train.shape)
+
+    dl_model.fit(x=X_train,y=y_train,
+            validation_data=(X_test,y_test),
+            batch_size=128,epochs=100)
+
+
+    loss_df = pd.DataFrame(dl_model.history.history)
+    fig = plt.figure()
+    plt.plot(loss_df)
+    plt.grid(True)
+    plt.title('loss function')
+    st.pyplot(fig)
+
+    return dl_model
